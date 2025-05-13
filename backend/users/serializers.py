f3d66ba9b9227a3cb83085374e1e4128cec5ddad
potyne <tinesarkiv@gmail.com>
2025-05-13 21:04:46 +0800

    serializers and views are up

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -27,15 +27,17 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username','email', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password',]
 
-        def create(self, validated_data):
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                password=validated_data['password']
-            )
-            return user
+    def create(self, validated_data):  
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+        return user
 
 
 # obtain token
--- backend/users/views.py
+++ backend/users/views.py
@@ -27,11 +27,14 @@ from rest_framework.decorators import api_view, permission_classes
 # jwt register view
 class RegisterUserView(APIView):
     def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
 
 
 # custom login view
